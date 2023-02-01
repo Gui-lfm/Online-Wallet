a8254277342553a63eca69d9e6664bb615665f69
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-02-01 16:27:39 -0300

    pagina de login criada

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-class Login extends React.Component {
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+    isSubmitBtnDisabled: true,
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({ [name]: value }, this.handleValidation);
+  };
+
+  handleValidation = () => {
+    const { email, password } = this.state;
+
+    const validations = {
+      email: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+      password: 6,
+    };
+
+    const isPasswordValid = password.length >= validations.password;
+    const isEmailValid = validations.email.test(email);
+
+    if (isPasswordValid && isEmailValid) {
+      this.setState({
+        isSubmitBtnDisabled: false,
+      });
+    } else {
+      this.setState({
+        isSubmitBtnDisabled: true,
+      });
+    }
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { dispatch, history } = this.props;
+    dispatch();
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, isSubmitBtnDisabled } = this.state;
+
+    return (
+      <form>
+        <input
+          value={ email }
+          name="email"
+          type="email"
+          data-testid="email-input"
+          onChange={ this.handleChange }
+        />
+        <input
+          value={ password }
+          name="password"
+          type="password"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="submit "
+          disabled={ isSubmitBtnDisabled }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
+      </form>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
+
+export default connect()(Login);
