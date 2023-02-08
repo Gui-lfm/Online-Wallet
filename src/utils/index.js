4216e493228f6082b281ce7ad8ebb3a7447af9c6
Guilherme Lucena <guilherme.lucena17@gmail.com>
2023-02-07 21:40:46 -0300

    detalhes finais adicionados

---
+++ src/utils/index.js
@@ -0,0 +1,22 @@
+export const classInput = `rounded-md 
+border-2 border-blue-800 focus:outline-none text-neutral-900 p-2 w-full`;
+export const classForm = `bg-neutral-200 p-8 rounded-lg flex gap-y-5
+flex-wrap flex-col`;
+export const classTitle = 'text-2xl text-center text-blue-800';
+export const classLabel = 'text-blue-800 text-lg';
+export const classBtn = `bg-blue-800 
+p-3 rounded-md w-full text-neutral-50 hover:bg-blue-900 ease-in-out duration-300`;
+export const classContainer = 'flex items-center justify-center px-10 mb-5';
+
+// table
+
+export const classTable = 'w-96 mx-auto';
+export const classThead = 'border-b border-slate-600 bg-blue-800 p-2 text-white';
+export const classThCell = 'px-4 pt-2';
+export const classTbody = `border-t-4
+border-neutral-50 bg-neutral-300 text-sm text-center text-emerald-800`;
+
+export const classEditBtn = `bg-emerald-500 
+p-2 text-white mx-1 rounded-md hover:bg-emerald-700 ease-in-out duration-300`;
+export const classDeleteBtn = `bg-red-500 
+p-2 text-white mx-1 rounded-md hover:bg-red-700 ease-in-out duration-300`;
